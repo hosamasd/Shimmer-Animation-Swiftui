56d1dbe3effc699b965ed9ee6338585c8e34da9a
hosam mohamed <hosamelmalt@gmail.com>
2020-11-24 07:07:36 -0800

    Initial Commit

---
+++ Shimmer Animation/Shimmer_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shimmer_AnimationApp.swift
+//  Shimmer Animation
+//
+//  Created by Hossam on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct Shimmer_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
