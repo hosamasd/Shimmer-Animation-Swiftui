689142e2352d714b50460907d5cf4a3575dcfaa4
hosam <hosamelmalt@gmail.com>
2020-12-15 11:34:10 +0200

    images added

--- Shimmer Animation/Shimmer_AnimationApp.swift
+++ Shimmer Animation/Shimmer_AnimationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Shimmer_AnimationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentsView()
         }
     }
 }
---
+++ Shimmer Animation/scenes/ContentsView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentsView.swift
+//  Shimmer Animation
+//
+//  Created by hosam on 12/15/20.
+//
+
+import SwiftUI
+
+struct ContentsView: View {
+    var body: some View {
+       Home()
+    }
+}
+
+struct ContentsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentsView()
+    }
+}
